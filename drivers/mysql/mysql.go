bab682a8080f62c805ecc5fe107b679c732046c1
nadirbasalamah <nadir008basalamah@gmail.com>
2023-03-20 12:32:30 +0700

    feat(db): add method for creating database instance

---
+++ drivers/mysql/mysql.go
@@ -0,0 +1,45 @@
+package mysql_driver
+
+import (
+	"fmt"
+	"log"
+	"notes-api/drivers/mysql/categories"
+	"notes-api/drivers/mysql/notes"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type ConfigDB struct {
+	DB_USERNAME string
+	DB_PASSWORD string
+	DB_NAME     string
+	DB_HOST     string
+	DB_PORT     string
+}
+
+func (config *ConfigDB) InitDB() *gorm.DB {
+	var err error
+
+	var dsn string = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		config.DB_USERNAME,
+		config.DB_PASSWORD,
+		config.DB_HOST,
+		config.DB_PORT,
+		config.DB_NAME,
+	)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalf("error when connecting to the database: %s", err)
+	}
+
+	log.Println("connected to the database")
+
+	return db
+}
+
+func DBMigrate(db *gorm.DB) {
+	db.AutoMigrate(&notes.Note{}, &categories.Category{})
+}
