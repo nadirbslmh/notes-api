a71dba4f75f590049a504702ff28b4c8abe98e75
nadirbasalamah <nadir008basalamah@gmail.com>
2023-03-21 10:06:51 +0700

    feat(category): add controller for category

--- controllers/categories/http.go
+++ controllers/categories/http.go
@@ -1,6 +1,14 @@
 package categories
 
-import "notes-api/businesses/categories"
+import (
+	"net/http"
+	"notes-api/businesses/categories"
+	"notes-api/controllers"
+	"notes-api/controllers/categories/request"
+	"notes-api/controllers/categories/response"
+
+	"github.com/labstack/echo/v4"
+)
 
 type CategoryController struct {
 	categoryUsecase categories.Usecase
@@ -12,4 +20,81 @@ func NewCategoryController(categoryUC categories.Usecase) *CategoryController {
 	}
 }
 
-//TODO: implement this
+func (ctrl *CategoryController) GetAllCategories(c echo.Context) error {
+	ctx := c.Request().Context()
+
+	categoriesData, err := ctrl.categoryUsecase.GetAll(ctx)
+
+	if err != nil {
+		return controllers.NewResponse(c, http.StatusInternalServerError, "failed", "error when fetching data", "")
+	}
+
+	categories := []response.Category{}
+
+	for _, category := range categoriesData {
+		categories = append(categories, response.FromDomain(category))
+	}
+
+	return controllers.NewResponse(c, http.StatusOK, "success", "all categories", categories)
+}
+
+func (ctrl *CategoryController) CreateCategory(c echo.Context) error {
+	input := request.Category{}
+	ctx := c.Request().Context()
+
+	if err := c.Bind(&input); err != nil {
+		return controllers.NewResponse(c, http.StatusBadRequest, "failed", "validation failed", "")
+	}
+
+	err := input.Validate()
+
+	if err != nil {
+		return controllers.NewResponse(c, http.StatusBadRequest, "failed", "validation failed", "")
+	}
+
+	category, err := ctrl.categoryUsecase.Create(ctx, input.ToDomain())
+
+	if err != nil {
+		return controllers.NewResponse(c, http.StatusInternalServerError, "failed", "error when inserting data", "")
+	}
+
+	return controllers.NewResponse(c, http.StatusCreated, "success", "category created", response.FromDomain(category))
+}
+
+func (ctrl *CategoryController) UpdateCategory(c echo.Context) error {
+	input := request.Category{}
+	ctx := c.Request().Context()
+
+	if err := c.Bind(&input); err != nil {
+		return controllers.NewResponse(c, http.StatusBadRequest, "failed", "validation failed", "")
+	}
+
+	var id string = c.Param("id")
+
+	err := input.Validate()
+
+	if err != nil {
+		return controllers.NewResponse(c, http.StatusBadRequest, "failed", "validation failed", "")
+	}
+
+	category, err := ctrl.categoryUsecase.Update(ctx, id, input.ToDomain())
+
+	if err != nil {
+		return controllers.NewResponse(c, http.StatusInternalServerError, "failed", "error when updating data", "")
+	}
+
+	return controllers.NewResponse(c, http.StatusOK, "success", "category updated", response.FromDomain(category))
+}
+
+func (ctrl *CategoryController) DeleteCategory(c echo.Context) error {
+	var id string = c.Param("id")
+	ctx := c.Request().Context()
+
+	err := ctrl.categoryUsecase.Delete(ctx, id)
+
+	if err != nil {
+		return controllers.NewResponse(c, http.StatusInternalServerError, "failed", "error when deleting data", "")
+	}
+
+	return controllers.NewResponse(c, http.StatusOK, "success", "category deleted", "")
+}
